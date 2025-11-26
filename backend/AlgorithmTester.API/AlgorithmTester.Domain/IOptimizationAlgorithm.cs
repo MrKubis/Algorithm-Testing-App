87c0ebfcacaf94a91b7769dc7223df5051d194c0
MrKubis <jakub.f102@gmail.com>
2025-11-26 11:31:29 +0100

    Init Domain

---
+++ backend/AlgorithmTester.API/AlgorithmTester.Domain/IOptimizationAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AlgorithmTester.Domain
+{
+    public delegate double fitnessFunction(params double[] arg);
+    public interface IOptimizationAlgorithm
+    {
+        string Name { get; set; }
+        static void Solve(fitnessFunction f, double[] domain, params double[] parameters) { }
+        ParamInfo[] ParamsInfo { get; set; }
+        IStateWriter writer {  get; set; }
+        IStateReader reader { get; set; }
+        IGenerateTextReport stringReportGenerator { get; set; }
+        IGeneratePDFReport pdfReportGenerator { get; set; }
+        double[] XBest { get; set; }
+        double FBest { get; set; }
+        int NumberOfEvaluationFitnessFunction { get; set; }
+    }
+}
---
+++ backend/AlgorithmTester.API/AlgorithmTester.Domain/ParamInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTester.Domain
+{
+    public class ParamInfo
+    {
+        string Name {  get; set; }
+        string Description { get; set; }
+        double UpperBoundary { get; set; }
+        double LowerBoundary { get; set; }
+    }
+}
