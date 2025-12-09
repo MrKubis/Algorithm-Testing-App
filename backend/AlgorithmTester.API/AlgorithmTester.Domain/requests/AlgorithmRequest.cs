1e135ad7816f4e1fbf77b525def941fb56306630
MrKubis <jakub.f102@gmail.com>
2025-12-09 15:48:24 +0100

    Create AlgorithmRequest.cs

---
+++ backend/AlgorithmTester.API/AlgorithmTester.Domain/requests/AlgorithmRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTester.Domain.requests
+{
+    public class AlgorithmRequest
+    {
+        public string AlgorithmName { get; set; }
+        public List<ParamInfo> ParamInfoList { get; set; }
+        public int Step { get; set; }
+        public string[] FunctionList { get; set; }
+    }
+}
