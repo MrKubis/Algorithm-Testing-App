ef7703188eb0f4c6505cc294e1e3278b1d593876
MrKubis <jakub.f102@gmail.com>
2025-12-09 15:48:22 +0100

    Create AlgorithmCommand.cs

---
+++ backend/AlgorithmTester.API/AlgorithmTester.Domain/requests/AlgorithmCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTester.Domain.requests
+{
+    public class AlgorithmCommand
+    {
+        public string RequestedState { get; set; }
+    }
+
+    //To później
+    public enum State
+    {
+        START,STOP
+    }
+}
+
